3e16168ba2768a830836fc11ade46c236d4b46ec
Emily Loggins <em.loggins7@gmail.com>
2019-10-10 14:12:07 -0500

    boat list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   clicked = false;
-  
+
+  handleClick() {
+    this.clicked = true;
+  }
+
+  boats = [
+    {
+    name: 'Big Mama',
+    year: '1969',
+    img: '../../img/download.jpg'
+  },
+  {
+    name: 'The Girthy Gertrude',
+    year: '420',
+    img: '../../img/download.jpg'
+  },
+  {
+    name: 'Can\'t Believe it\'s not Butter!',
+    year: '666666',
+    img: '../../img/download.jpg'
+  }
+]
   constructor() { }
   ngOnInit() {
   }
